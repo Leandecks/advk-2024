2ed01ba8af7a85a5b92b178abf97e90a87990d03
Leandecks <leandrogridelli@gmail.com>
2024-11-25 20:55:14 +0100

    Add info, complete day 2, fix index.html language

--- doors/1/1.js
+++ doors/1/1.js
@@ -4,7 +4,7 @@ const container = document.querySelector(".container");
 const btn = document.getElementById("button");
 
 // CHANGE NAME HERE
-const correct = document.getElementById("Mexico");
+const correct = document.getElementById("9");
 // CHANGE NAME HERE
 
 const text = document.createElement("p");
@@ -15,8 +15,12 @@ text.style.color = "white";
 const letterEl = document.createElement("button");
 
 // CHANGE LETTER HERE
-const letter = "F";
+const letter = "Fr";
 // CHANGE LETTER HERE
+
+// CHANGE INFO HERE
+const info = `Deutschland grenzt an die Schweiz, Österreich, Tscheschien, Polen, Dänemark, die Niederlande, Belgien, Luxemburg und Frankreich.`;
+// CHANGE INFO HERE
 
 letterEl.style.border = "none";
 
@@ -28,7 +32,7 @@ btn.addEventListener("click", () => {
     text.textContent = "Richtige Antwort!";
     correct.parentElement.appendChild(text);
 
-    letterEl.textContent = `Herzlichen Glückwunsch, du hast das Rätsel gelöst! Der heutige Buchstabe ist "${letter}"`;
+    letterEl.textContent = `Herzlichen Glückwunsch, du hast das Rätsel gelöst! ${info} Der heutige Buchstabe ist "${letter}"`;
     letterEl.style.backgroundColor = "#2a7b6f";
     container.appendChild(letterEl);
   } else {
@@ -38,7 +42,7 @@ btn.addEventListener("click", () => {
     text.textContent = "...wäre korrekt gewesen!";
     correct.parentElement.appendChild(text);
 
-    letterEl.textContent = `Leider konntest du das heutige Rätsel nicht lösen! Behalte im Kopf dass man das finale Lösungswort auch ohne jeden einzelnen Buchstaben finden kann!`;
+    letterEl.textContent = `Leider konntest du das heutige Rätsel nicht lösen! ${info} Behalte im Kopf dass man das finale Lösungswort auch ohne jeden einzelnen Buchstaben finden kann!`;
     letterEl.style.backgroundColor = "#964a50";
     container.appendChild(letterEl);
   }
--- doors/2/2.js
+++ doors/2/2.js
@@ -1,20 +1,49 @@
 "use strict";
-console.log("JS Started");
 
-const submit = document.querySelector('button');
-const content = document.querySelector('.content');
-const display = document.createElement("p");
+const container = document.querySelector(".container");
+const btn = document.getElementById("button");
 
-submit.addEventListener("click", () => {
-  if (document.querySelector("#Mexico").checked) {
-    display.textContent = "Giusto!";
-    display.style.color = "green";
-    display.style.marginBottom = ".5rem";
-    content.appendChild(display);
+// CHANGE NAME HERE
+const correct = document.getElementById("11");
+// CHANGE NAME HERE
+
+const text = document.createElement("p");
+text.style.display = "inline-block";
+text.style.margin = "0 0 0 1rem";
+text.style.color = "white";
+
+const letterEl = document.createElement("button");
+
+// CHANGE LETTER HERE
+const letter = "Fr";
+// CHANGE LETTER HERE
+
+// CHANGE INFO HERE
+const info = `La Francia continentale confina con il Belgio (bestia), la Germania, la Spagna, la Svizzera, il Lussemburgo, l'Italia, Monaco e l'Andorra. La Guiana Francese confina con il Brasile e il Suriname. Saint Martin confina con Sint Maarten che è olandese.`;
+// CHANGE INFO HERE
+
+letterEl.style.border = "none";
+
+btn.addEventListener("click", () => {
+  if (correct.checked) {
+    correct.setAttribute("aria-invalid", "false");
+    correct.parentElement.style.background = "#2a7b6f";
+
+    text.textContent = "Risposta corretta!";
+    correct.parentElement.appendChild(text);
+
+    letterEl.textContent = `Congrutalulazioni, hai risolto il quiz! ${info} La lettera di oggi è "${letter}"`;
+    letterEl.style.backgroundColor = "#2a7b6f";
+    container.appendChild(letterEl);
   } else {
-    display.textContent = "Sbagliato!";
-    display.style.color = "red";
-    display.style.marginBottom = ".5rem";
-    content.append(display);
+    correct.setAttribute("aria-invalid", "true");
+    correct.parentElement.style.background = "#964a50";
+
+    text.textContent = "...sarebbe stato corretto!";
+    correct.parentElement.appendChild(text);
+
+    letterEl.textContent = `Purtroppo oggi non ce l'hai fatta a risolvere il quiz! ${info} Ricordati che puoi indovinare la parola finale anche senza risolvere ogni singolo quiz...`;
+    letterEl.style.backgroundColor = "#964a50";
+    container.appendChild(letterEl);
   }
 });
