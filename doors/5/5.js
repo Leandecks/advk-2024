f922b5cca280f2fefd105db96404d8365b17d60d
Leandecks <leandrogridelli@gmail.com>
2024-11-28 21:29:44 +0100

    Days five and six

--- doors/5/5.js
+++ doors/5/5.js
@@ -1,33 +1,11 @@
-"use strict";
-console.log("JS Started");
+import validateDe from "../doors_de";
 
-const submit = document.querySelector('button');
-const content = document.querySelector('.content');
-const display = document.createElement("p");
+const container = document.querySelector(".container");
+const btn = document.getElementById("button");
 
-submit.addEventListener("click", () => {
-  if (document.getElementById("2").checked
-      && document.getElementById("4").checked
-      && !document.getElementById("1").checked
-      && !document.getElementById("3").checked) {
-    display.textContent = "Korrekt!";
-    display.style.color = "green";
-    display.style.marginBottom = ".5rem";
-    content.appendChild(display);
-  } else if (document.getElementById("1").checked || document.getElementById("3").checked) {
-    display.textContent = "Falsch!";
-    display.style.color = "red";
-    display.style.marginBottom = ".5rem";
-    content.append(display);
-  } else if (document.getElementById("2").checked || document.getElementById("4").checked) {
-    display.textContent = "Es fehlt noch eine Antwort!";
-    display.style.color = "rgb(237, 182, 0)";
-    display.style.marginBottom = ".5rem";
-    content.appendChild(display);
-  } else {
-    display.textContent = "Falsch!";
-    display.style.color = "red";
-    display.style.marginBottom = ".5rem";
-    content.append(display);
-  }
-});
+const letter = "ä";
+const id = "2";
+const info =
+  "Bei der tiefen Ausatmung wird zusätzlich die Muskelgruppe der Musculus intercostalis externus aktiviert.";
+
+validateDe(id, letter, info, container, btn);
