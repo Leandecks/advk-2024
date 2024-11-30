cebf4ff9a4502a671e0cc84b6d4f4d40e2602a7e
Leandecks <leandrogridelli@gmail.com>
2024-11-30 20:49:12 +0100

    Changes for building properly with vite

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import path from "path";
+
+export default {
+  base: "/advk-2024/",
+  build: {
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "index.html"),
+        "doors/1": path.resolve(__dirname, "doors/1/1.html"),
+        "doors/2": path.resolve(__dirname, "doors/2/2.html"),
+        "doors/3": path.resolve(__dirname, "doors/3/3.html"),
+        "doors/4": path.resolve(__dirname, "doors/4/4.html"),
+        "doors/5": path.resolve(__dirname, "doors/5/5.html"),
+        "doors/6": path.resolve(__dirname, "doors/6/6.html"),
+        "doors/7": path.resolve(__dirname, "doors/7/7.html"),
+        "doors/8": path.resolve(__dirname, "doors/8/8.html"),
+        "doors/9": path.resolve(__dirname, "doors/9/9.html"),
+        "doors/10": path.resolve(__dirname, "doors/10/10.html"),
+        "doors/11": path.resolve(__dirname, "doors/11/11.html"),
+        "doors/12": path.resolve(__dirname, "doors/12/12.html"),
+        "doors/13": path.resolve(__dirname, "doors/13/13.html"),
+        "doors/14": path.resolve(__dirname, "doors/14/14.html"),
+        "doors/15": path.resolve(__dirname, "doors/15/15.html"),
+        "doors/16": path.resolve(__dirname, "doors/16/16.html"),
+        "doors/17": path.resolve(__dirname, "doors/17/17.html"),
+        "doors/18": path.resolve(__dirname, "doors/18/18.html"),
+        "doors/19": path.resolve(__dirname, "doors/19/19.html"),
+        "doors/20": path.resolve(__dirname, "doors/20/20.html"),
+        "doors/21": path.resolve(__dirname, "doors/21/21.html"),
+        "doors/22": path.resolve(__dirname, "doors/22/22.html"),
+        "doors/23": path.resolve(__dirname, "doors/23/23.html"),
+        "doors/24": path.resolve(__dirname, "doors/24/24.html"),
+      },
+    },
+  },
+};
