457ae1e82af6b760254a1f90bde80e96f60d368f
Leandecks <leandrogridelli@gmail.com>
2024-12-12 21:34:38 +0100

    Delete dist

--- dist/assets/doors/8-iqLGcugA.js
+++
@@ -1,1 +0,0 @@
-import"../saving-dRy0D6rM.js";import"../foundLetters-C8hbrHsr.js";import{v as t}from"../doors_it-cpHv46BM.js";const e=document.querySelector(".container"),n=document.getElementById("button"),o="ä",a="3",i="La musica leggera nella grande maggioranza dei casi segue il ritmo tum-ta-tum-ta.";t(a,o,i,e,n,8);
