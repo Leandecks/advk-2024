9f55f39ee6b5c21d114aaebb4c9120f0fa49bfb7
Leandecks <leandrogridelli@gmail.com>
2024-12-12 21:31:51 +0100

    Days 12, 14

--- doors/13/13.js
+++ doors/13/13.js
@@ -1,20 +1,14 @@
-"use strict";
-console.log("JS Started");
+import validateDe from "../doors_de";
 
-const submit = document.querySelector('button');
-const content = document.querySelector('.content');
-const display = document.createElement("p");
+const container = document.querySelector(".container");
+const btn = document.getElementById("button");
 
-submit.addEventListener("click", () => {
-  if (document.getElementById("1").checked) {
-    display.textContent = "Korrekt!";
-    display.style.color = "green";
-    display.style.marginBottom = ".5rem";
-    content.appendChild(display);
-  } else {
-    display.textContent = "Falsch!";
-    display.style.color = "red";
-    display.style.marginBottom = ".5rem";
-    content.append(display);
-  }
-});
+const letter = "o";
+const id = "4";
+const info =
+  "Python wird für generelle Programmierung benutzt, im Internet vor allem für Serverprozesse. " +
+  "Java wird gerne für Anwendungen benutzt. " +
+  "Kotlin ist ein modernes Java. " +
+  "HTML, CSS und JavaScript sind die 3 Hauptsprachen des Internets.";
+
+validateDe(id, letter, info, container, btn, 7);
--- doors/14/14.js
+++ doors/14/14.js
@@ -1,20 +1,14 @@
-"use strict";
-console.log("JS Started");
+import validateIt from "../doors_it";
 
-const submit = document.querySelector('button');
-const content = document.querySelector('.content');
-const display = document.createElement("p");
+const container = document.querySelector(".container");
+const btn = document.getElementById("button");
 
-submit.addEventListener("click", () => {
-  if (document.getElementById("1").checked) {
-    display.textContent = "Corretto!";
-    display.style.color = "green";
-    display.style.marginBottom = ".5rem";
-    content.appendChild(display);
-  } else {
-    display.textContent = "Sbagliato!";
-    display.style.color = "red";
-    display.style.marginBottom = ".5rem";
-    content.append(display);
-  }
-});
+const letter = "o";
+const id = "4";
+const info =
+  "Python viene utilizzato per programmazione generale. " +
+  "Java è un'isola (quando non è un'isola viene utilizzato per sviluppare programmi e applicazioni). " +
+  "Kotlin è Java moderno (non l'isola). " +
+  "HTML, CSS e JavaScript vengono utilizzati per sviluppare in internet.";
+
+validateIt(id, letter, info, container, btn, 8);
