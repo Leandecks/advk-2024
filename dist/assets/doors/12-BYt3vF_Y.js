da0dea6ce28cd67404bb418d6391c115d87f528d
Leandecks <leandrogridelli@gmail.com>
2024-12-12 21:32:44 +0100

    Days 13, 14

---
+++ dist/assets/doors/12-BYt3vF_Y.js
@@ -0,0 +1,1 @@
+import"../saving-dRy0D6rM.js";import"../foundLetters-C8hbrHsr.js";import{v as t}from"../doors_it-cpHv46BM.js";const o=document.querySelector(".container"),n=document.getElementById("button"),c="J",e="4",i="Il blocco non ha ostacoli se non ci sono attriti.";t(e,c,i,o,n,8);
