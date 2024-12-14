1432d20da6ede54aea59d15e31f93879320d1a1c
Leandecks <leandrogridelli@gmail.com>
2024-12-14 21:00:36 +0100

    Days 15, 16, 17, 18

---
+++ dist/assets/doors/11-DXjQ6SaN.js
@@ -0,0 +1,1 @@
+import"../saving-dRy0D6rM.js";import"../foundLetters-CmyvoFxs.js";import{v as t}from"../doors_de-Cm6x-mRx.js";const n=document.querySelector(".container"),e=document.getElementById("button"),o="J",c="4",i="Nichts kann den Block aufhalten, wenn es keine Reibung gibt.";t(c,o,i,n,e,11);
---
+++ dist/assets/doors/6-CD_ZmNmZ.js
@@ -0,0 +1,1 @@
+import"../saving-dRy0D6rM.js";import"../foundLetters-CmyvoFxs.js";import{v as t}from"../doors_it-DlLmd5Fj.js";const n=document.querySelector(".container"),o=document.getElementById("button"),e="ä",i="2",r="Durante un'inspirazione profonda attiviamo anche i muscoli intercostali esterni.";t(i,e,r,n,o,6);
